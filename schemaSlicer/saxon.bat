471ce0b2afe0edb9dbaa601b08b60693a18db52f
brandon.duffy <brandon.duffy@stillwater.com>
2017-01-19 15:19:04 -0600

    Added "Policy Endorsement" xsd slice. Reorganized schemaSlicer directory

--- schemaSlicer/saxon.bat
+++ schemaSlicer/saxon.bat
@@ -3,11 +3,17 @@ echo off
 
 rem java -Xmx1024m -Xms512m -cp .\Saxon6.5.5\saxon.jar com.icl.saxon.StyleSheet -?
 
-copy messages_home_quote_1.0.xml messages.xml
-java -Xmx1024m -Xms512m -cp .\Saxon6.5.5\saxon.jar com.icl.saxon.StyleSheet -t -o home-quote-v1.0.xsd  acord-pcs-v1_26_0-ns-nodoc-codes.xsd PCS-schemaMessageExtract.xsl
+set dir=home_quote_1_0-slice
+copy %dir%\messages_home_quote_1.0.xml messages.xml
+java -Xmx1024m -Xms512m -cp .\Saxon6.5.5\saxon.jar com.icl.saxon.StyleSheet -t -o %dir%\home-quote-v1.0.xsd  acord-pcs-v1_26_0-ns-nodoc-codes.xsd PCS-schemaMessageExtract.xsl
 
-copy messages_auto_quote_1.0.xml messages.xml
-java -Xmx1024m -Xms512m -cp .\Saxon6.5.5\saxon.jar com.icl.saxon.StyleSheet -t -o auto-quote-v1.0.xsd  acord-pcs-v1_26_0-ns-nodoc-codes.xsd PCS-schemaMessageExtract.xsl
+set dir=auto_quote_1_0-slice
+copy %dir%\messages_auto_quote_1.0.xml messages.xml
+java -Xmx1024m -Xms512m -cp .\Saxon6.5.5\saxon.jar com.icl.saxon.StyleSheet -t -o %dir%\auto-quote-v1.0.xsd  acord-pcs-v1_26_0-ns-nodoc-codes.xsd PCS-schemaMessageExtract.xsl
+
+set dir=policy_endorse-slice
+copy %dir%\messages_policy_endorsement.xml messages.xml
+java -Xmx1024m -Xms512m -cp .\Saxon6.5.5\saxon.jar com.icl.saxon.StyleSheet -t -o %dir%\policy-endorsement.xsd  acord-pcs-v1_26_0-ns-nodoc-codes.xsd PCS-schemaMessageExtract.xsl
 
 del messages.xml
 
